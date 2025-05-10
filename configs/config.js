c69a185b295e231cfe1fef483392a34194af11bd
ArturRushMFM <artur@mfmsoft.com>
2025-05-10 14:38:21 +0400

    Updated the method for reading Firebase configurations

--- configs/config.js
+++ configs/config.js
@@ -4,4 +4,19 @@ module.exports = {
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRES_IN: parseInt(process.env.JWT_EXPIRES_IN) || "1h",
+
+  // NOTE: Firebase configs
+  firebaseConfigs: {
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: process.env.universe_domain,
+  },
 };
--- configs/firebaseInstance.js
+++ configs/firebaseInstance.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../admin.json");
+// const serviceAccount = require("../admin.json");
+
+const { firebaseConfigs } = require("../configs/config");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseConfigs),
 });
 
 const db = admin.firestore();
